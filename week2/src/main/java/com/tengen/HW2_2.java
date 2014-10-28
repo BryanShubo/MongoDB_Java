549135fd6a80fb93cc70e18fb350344074d15548
Bryan <bryanshubo@gmail.com>
2014-10-28 11:22:49 -0400

    HW2_2.java
    homework 2-2

---
+++ week2/src/main/java/com/tengen/HW2_2.java
@@ -0,0 +1,44 @@
+package com.tengen;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.QueryBuilder;
+import com.mongodb.ServerAddress;
+
+import java.net.UnknownHostException;
+
+public class HW2_2 {
+
+	public static void main(String[] args) throws UnknownHostException {
+		MongoClient client = new MongoClient(new ServerAddress("localhost",
+				27017));
+
+		DB database = client.getDB("students");
+		DBCollection collection = database.getCollection("grades");
+		
+		for (int i = 0; i < 200; i++) {
+			QueryBuilder builder = QueryBuilder.start("student_id").is(i).and("type").is("homework");
+			DBCursor cursor = collection.find(builder.get()).sort(new BasicDBObject("student_id",1).append("score",1));
+			
+			try {
+				while (cursor.hasNext()) {
+
+					DBObject cur = cursor.next();
+					collection.remove(cur);
+					break;
+				}
+			} finally {
+				cursor.close();
+			}
+		}
+		
+		System.out.println(collection.count());
+		
+
+	}
+
+}
